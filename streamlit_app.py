6bfbd436803ff85d523b5fd3285bfa0d73438b2a
DaYoung Jung <dayoung.jung0102@gmail.com>
2026-01-29 22:51:36 +0900

    Update app title and description in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+st.title('🤖 서버 실시간 이상 탐지 대시보드 🤖')
 
-st.write('Hello world!')
+st.info('이 앱은 머신러닝 모델을 활용하여 실시간으로 서버 상태를 모니터링할 수 있게 시각화한 대시보드를 제공합니다')
